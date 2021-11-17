e518e10ba20bae0fda9ae6f746dfc85e30984067
Davi Fontes da Silva Faistauer Mendes <79602569+davizin04@users.noreply.github.com>
2021-11-17 16:38:35 -0300

    Add files via upload

---
+++ saida.js
@@ -0,0 +1,8 @@
+
+alert("Olá Davi, vamos aprender JS!!!");
+
+console.log("Salve Sandro");
+
+document.getElementById("texto").innerHTML = "Salve, Mundo";
+
+document.write("Aprendendo JavaScript, estou muito animado!");
---
+++ script.js
@@ -0,0 +1,49 @@
+/** Atividade 1 */
+var nome = "Davi";
+alert(nome);
+
+/** Atividade 2 */
+var positivo = 8;
+var negativo = 29;
+
+soma = positivo + negativo;
+
+alert(soma);
+
+/** Atividade 3 */
+var positivo = 12;
+var negativo = 815;
+
+multiplicação = positivo * negativo;
+
+document.write(multiplicação);
+
+/** Atividade 4 */
+var completar = prompt("Digite seu nome");
+document.write (" Bem vindo, ", nome);
+
+/** Atividade 5 */
+var nota1 = 2;
+var nota2 = 3;
+var nota3 = 5;
+
+somatória = nota1 + nota2+ nota3;
+média = somatória / 3;
+
+/** Atividade 6 */
+var n1 = prompt("Número de sua escolha");
+
+dobro = n1 * 2;
+
+/** Atividade 7 */
+var lado = prompt("tamanho lado");
+var altura = prompt("tamanho altura");
+
+área = lado * altura;
+
+/** Atividade 8 */
+var string1 = parseInt("51");
+var string2 = parseInt("14");
+
+soma = string1 + string2
+
